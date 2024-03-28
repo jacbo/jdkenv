0def33dd3cf0f661a079267b3583530ef1e4d0da
songmengqiang <a@qq.com>
2024-03-28 11:42:24 +0800

    add bat file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jdkenv
 切换jdk环境
+
+将bat/env.bat放入jdk父级目录
+添加path选项指向env.bat以便可以在任意位置访问env命令
---
+++ bat/env.bat
@@ -0,0 +1,22 @@
+@echo off
+set basepath=D:\NRZ\IDE\java\
+setlocal enabledelayedexpansion 
+
+echo %basepath% 
+set "counter=1"
+for /d  %%F in (%basepath%*) do (  
+    echo !counter! : %%F  
+	set /a "counter+=1"
+)
+set /p input="选择环境:"
+set /a "counter=1"
+for /d  %%F in (%basepath%*) do ( 
+	if !counter! == %input% (
+		set "JAVA_HOME=%%F"
+		goto :endloop
+	)
+	set /a "counter+=1"
+)
+:endloop
+endlocal & set JAVA_HOME=%JAVA_HOME% & set PATH=%JAVA_HOME%\bin;%PATH%
+echo 选择的环境 %JAVA_HOME%
